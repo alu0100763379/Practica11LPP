d8a555d9efd98f73028c736eea4b3b5b4957de65
hectorinoman <hectorinoman@gmail.com>
2015-11-03 15:55:00 +0000

    principio de la clase

--- lib/referencia.rb
+++ lib/referencia.rb
@@ -1,5 +1,22 @@
 require "referencia/version"
 
 module Referencia
-  # Your code goes here...
+    
+    attr_reader :autor, :titulo, :serie, :editorial, :edicion, :fecha, :isbn
+    
+    def initialize(autor, titulo, serie, editorial, edicion, fecha, isbn)
+        @autor = autor;
+        @titulo = titulo;
+        @serie = serie;
+        @editorial = editorial;
+        @edicion = edicion;
+        @fecha = fecha;
+        @isbn = isbn;
+    end
+    
+    def to_s
+    "(#{@autor},#{@titulo},#{@serie},#{@editorial},#{@edicion},#{@fecha},#{@isbn})"
+    end
+    
+    
 end
