209368d54338da627a3d9a034f189e849a08411e
alu0100763379 <alu0100@ull.edu.es>
2015-11-03 17:16:32 +0000

    mejorando la estructura

--- lib/referencia.rb
+++ lib/referencia.rb
@@ -1,22 +1,5 @@
-#require "referencia/version"
+require "referencia/version"
 
-class Referencia
-    
-    attr_reader :autor, :titulo, :serie, :editorial, :edicion, :fecha, :isbn
-    
-    def initialize(autor, titulo, serie, editorial, edicion, fecha, isbn)
-        @autor = autor
-        @titulo = titulo
-        @serie = serie
-        @editorial = editorial
-        @edicion = edicion
-        @fecha = fecha
-        @isbn = isbn
-    end
-    
-    def to_s
-    "(#{@autor},#{@titulo},#{@serie},#{@editorial},#{@edicion},#{@fecha},#{@isbn})"
-    end
-    
+module Referencia
     
 end
