5dccf003388f6ac260c450c8b6c06588964e4b60
hectorinoman <hectorinoman@gmail.com>
2015-11-04 17:49:53 +0000

    finalizacion de los test debe existir

--- spec/referencia_spec.rb
+++ spec/referencia_spec.rb
@@ -24,6 +24,20 @@ describe Referencia do
     it "Debe existir una serie" do
       expect(@referencia.serie).to eq("443")
     end
+    
+    it "Debe existir una editorial" do
+      expect(@referencia.editorial).to eq("Alpachino")
+    end
+    it "Debe existir un numero de edicion" do
+      expect(@referencia.edicion).to eq("Primera")
+    end
+    it "Debe existir una fecha de publicacion" do
+      expect(@referencia.fecha).to eq("2015")
+    end
+    it "Debe existir uno o mas numeros isnb" do
+      @referencia.isbn.length.should_not be 0
+    end
+    
   end
   
 end
