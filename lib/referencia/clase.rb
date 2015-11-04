0f29129e3c61108c0e4a63f8cf321ecc32700806
alu0100763379 <alu0100@ull.edu.es>
2015-11-04 17:45:39 +0000

    fallo sin arreglo

--- lib/referencia/clase.rb
+++ lib/referencia/clase.rb
@@ -16,5 +16,39 @@ class Bibliografia
         "(#{@autor},#{@titulo},#{@serie},#{@editorial},#{@edicion},#{@fecha},#{@isbn})"
     end
     
+    def get_autores
+      # Devuelve el listado de autores
+      @autor
+  end
+  
+  def get_titulo
+      # Devuelve el título
+      @titulo
+  end
+  
+  def get_serie
+      # Devuelve la serie
+      @serie
+  end
+  
+  def get_editorial
+      # Devuelve la editorial
+      @editorial
+  end
+  
+  def get_numero_edicion
+      # Devuelve el número de edición
+      @edicion
+  end
+  
+  def get_fecha_publicacion
+      # Devuelve la fecha de publicación
+      @fecha
+  end
+  
+  def get_isbn
+      # Devuelve el listado de números ISBN
+      @isbn
+  end
 end
     
--- spec/referencia_spec.rb
+++ spec/referencia_spec.rb
@@ -5,15 +5,37 @@ describe Referencia do
   
 
       before :each do
-        @referencia = Bibliografia.new("Hector","La sala de informatica","443","Alpachino","Primera","2015","20")
+        autores = [ 'Dave Thomas', 'Andy Hunt', 'Chad Fowler' ]
+        titulo = 'Programming Ruby 1.9 & 2.0: The Pragmatic Programmers Guide'
+        serie = '(The Facets of Ruby)'
+        editorial = 'Pragmatic Bookshelf'
+        edicion = '4 edition'
+        fecha_publicacion = 'July 7, 2013'
+        isbn = [ 'ISBN-13: 978-1937785499', 'ISBN-10: 1937785491']
+        #@referencia = Bibliografia.new("[Hector]","La sala de informatica","443","Alpachino","Primera","2015","20")
+        @r1 = Bibliografia.new(autores, titulo, serie, editorial, edicion, fecha_publicacion, isbn)
       end
 
-  describe 'Autor' do
-    it "Constructor" do
-      expect(@referencia).to eq("Hector","La sala de informatica","443","Alpachino","Primera","2015","20")
+  #describe 'Autor' do
+   # it "Constructor" do
+      #expect(@referencia).to eq("Hector","La sala de informatica","443","Alpachino","Primera","2015","20")
       #(@referencia.autor).should eql("Hector")
       #@referencia.should be_an_instance_of Bibliografia
+  #  end
+ # end
+  
+  describe "# Almacenamiento correcto de los atributos" do
+    it "Debe existir uno o más autores" do
+      @r1.get_autores.length.should_not be 0
     end
   end
   
+  describe "# Existen los métodos de obtención de los atributos" do
+    it "Existe un método para obtener el listado de autores" do
+      @r1.respond_to?(:get_autores).should be true
+    end
+  end
+  it "Existe un método para obtener el título" do
+      @r1.respond_to?(:get_titulo).should be true
+  end
 end
