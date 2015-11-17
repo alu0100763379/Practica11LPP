9af2fde25e34d5595937704f93832611058baab4
alu0100763379 <alu0100@ull.edu.es>
2015-11-17 19:16:55 +0000

    Eliminados los metodos de la clase Bibliografía para usar attr_accessor

--- lib/referencia/clase.rb
+++ lib/referencia/clase.rb
@@ -1,6 +1,6 @@
 class Bibliografia
     
-    #attr_accessor :autor, :titulo, :serie, :editorial, :edicion, :fecha, :isbn
+    attr_accessor :autor, :titulo, :serie, :editorial, :edicion, :fecha, :isbn
     
     def initialize(autor, titulo, serie, editorial, edicion, fecha, isbn)
         @autor = autor
@@ -11,38 +11,6 @@ class Bibliografia
         @fecha = fecha
         @isbn = isbn
     end
-    
-    def to_s
-        "(#{@autor},#{@titulo},#{@serie},#{@editorial},#{@edicion},#{@fecha},#{@isbn})"
-    end
-
-    def autor
-      @autor
-    end
-  
-  def titulo
-      @titulo
-  end
-  
-  def serie
-      @serie
-  end
-  
-  def editorial
-      @editorial
-  end
-  
-  def edicion
-      @edicion
-  end
-  
-  def fecha
-      @fecha
-  end
-  
-  def isbn
-      @isbn
-  end
 
 end
     
