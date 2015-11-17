5bedb3248ff2c1bc26a159f7139ed0119fb69a60
alu0100763379 <alu0100@ull.edu.es>
2015-11-17 19:29:35 +0000

    Modificada la clase lista para representar listas enlazadas

--- lib/referencia/lista.rb
+++ lib/referencia/lista.rb
@@ -1,22 +1,43 @@
 #Clase para representar la Lista
 
-Nodo = Struct.new(:value, :next_node)
+Nodo = Struct.new(:value, :next_node, :prev_node)
 
 class Lista
     
-   attr_accessor :head, :value, :next_node
+   attr_accessor :head, :value, :next_node, :tail, :prev_node
 
     def initialize
         @head = Nodo.new(nil)
+        @tail = @head
     end
-
-    def add(nodo)
+    
+    def add_primer_nodo(nodo)
         nodo.next_node = @head
+        nodo.prev_node = @tail
+        @head = nodo
+        @tail = nodo
+    end
+    
+    def add_principio(nodo)
+        nodo.next_node = @head
+        @head.prev_node = nodo
         @head = nodo
     end
 
-    def remove
+    def borrar_principio
         @head = @head.next_node
-    end 
-
+        @head.prev_node = nil
+    end
+    
+    def borrar_final
+        @tail = @tail.prev_node
+        @tail.next_node = nil
+    end
+    
+    def add_final(nodo)
+        nodo.prev_node = @tail
+        @tail.next_node = nodo
+        @tail = nodo
+    end
+    
 end
