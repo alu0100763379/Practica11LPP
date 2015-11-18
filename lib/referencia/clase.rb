62579ee102c8c8c5223cf6955c97db43543f94c3
alu0100763379 <alu0100@ull.edu.es>
2015-11-18 12:10:56 +0000

    Herencia Creada (Error en los Tests)

--- lib/referencia/clase.rb
+++ lib/referencia/clase.rb
@@ -1,16 +1,42 @@
 class Bibliografia
     
-    attr_accessor :autor, :titulo, :serie, :editorial, :edicion, :fecha, :isbn
+    attr_accessor :autor, :titulo, :fecha
     
-    def initialize(autor, titulo, serie, editorial, edicion, fecha, isbn)
+    def initialize(autor, titulo, fecha)
         @autor = autor
         @titulo = titulo
+        @fecha = fecha
+    end
+
+end
+
+class Libro < Bibliografia
+    
+    attr_accessor :serie, :editorial, :edicion, :isbn
+    
+    def initialize(serie, editorial, edicion, isbn)
         @serie = serie
         @editorial = editorial
         @edicion = edicion
-        @fecha = fecha
         @isbn = isbn
     end
+end
 
+class Revista < Bibliografia
+    
+    attr_accessor :isnn
+    
+    def initialize(isnn)
+        @isnn = issn
+    end
+end
+
+class Electronico < Bibliografia
+    
+    attr_accessor :url
+    
+    def initialize(url)
+        @url = url
+    end
 end
     
--- spec/referencia_spec.rb
+++ spec/referencia_spec.rb
@@ -7,17 +7,13 @@ describe Referencia do
     
         @lista = Lista.new()
         
-        @r1 = Bibliografia.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"], "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers Guide", "The Facets of Ruby", "Pragmatic Bookshelf", "4 edition", "July 7, 2013",["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"])
-        @r2 = Bibliografia.new(["Scott Chacon"], "Pro Git 2009th Edition", "Pro", "Apress", "2009 edition", "August 27, 2009", ["ISBN-13: 978-1430218333", "ISBN-10: 1430218339"])
-        @r3 = Bibliografia.new(["David Flanagan", "Yukihiro Matsumoto"], "The Ruby Programming Language", "Serie", "O’Reilly Media", " 1 edition", "February 4, 2008", ["ISBN-10: 0596516177", "ISBN-13: 978-0596516178"])
-        @r4 = Bibliografia.new(["David Chelimsky", "Dave Astels", " Bryan Helmkamp", "Dan North", "Zach Dennis", "Aslak Hellesoy"], "The RSpec Book: Behaviour Driven Development with RSpec, Cucumber, and Friends", "The Facets of Ruby", "Pragmatic Bookshelf", "1 edition", "December 25, 2010", ["ISBN-10: 1934356379", "ISBN-13: 978-1934356371",])
-        @r5 = Bibliografia.new(["Richard E. Silverman"], "Git Pocket Guide", "Serie", "O’Reilly Media", "1 edition",  "August 2, 2013", ["ISBN-10: 1449325866", "ISBN-13: 978-1449325862"])    
-        
+        @r1 = Libro.new(["Dave Thomas", "Andy Hunt", "Chad Fowler"], "Programming Ruby 1.9 & 2.0: The Pragmatic Programmers Guide", "The Facets of Ruby", "Pragmatic Bookshelf", "4 edition", "July 7, 2013",["ISBN-13: 978-1937785499", "ISBN-10: 1937785491"])
+        @r2 = Revista.new(["Scott Chacon"], "Pro Git 2009th Edition", "August 27, 2009", ["ISSN-13: 978-1430218333", "ISSN-10: 1430218339"])
+        @r3 = Electronico.new(["David Flanagan", "Yukihiro Matsumoto"], "The Ruby Programming Language", "February 4, 2008", "http://0596516177IS978_0596516178")
+              
         @n1 = Nodo.new(@r1)
         @n2 = Nodo.new(@r2)
         @n3 = Nodo.new(@r3)
-        @n4 = Nodo.new(@r4)
-        @n5 = Nodo.new(@r5)
         
   end
 
@@ -43,7 +39,7 @@ describe Referencia do
   
   it "Se extrae el primer elemento de la lista." do
     @lista.add_primer_nodo(@n1)
-    @lista.add_principio(@n4)
+    @lista.add_principio(@n2)
     @lista.borrar_principio
     expect(@lista.head).to eq(@n1)
     expect(@n1.value).to eq(@r1)
@@ -51,7 +47,7 @@ describe Referencia do
   
   it "Se extrae el último elemento de la lista." do
     @lista.add_primer_nodo(@n1)
-    @lista.add_final(@n5)
+    @lista.add_final(@n3)
     @lista.borrar_final
     expect(@lista.head).to eq(@n1)
     expect(@n1.value).to eq(@r1)
