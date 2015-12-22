4644c4f4d7452beda600ed26073c05023823254b
alu0100763379 <alu0100@ull.edu.es>
2015-12-22 21:00:45 +0000

    Creados los tests para la clase Libro

--- spec/referencia_spec.rb
+++ spec/referencia_spec.rb
@@ -20,3 +20,45 @@ describe Referencia do
     expect(@referencia.titulo).to eq('Universo Ruby')
   end
 end
+
+describe Libro do
+  before :all do
+    @libro = Libro.new('Programacion y Git') do
+      autores 'Carmen Trujillo'
+      fecha_publicacion 'Junio 21, 2014'
+      serie 'Programacion 1'
+     editorial 'Almac'
+      edicion 'Tercera'
+      isbn '278-553'
+    end
+  end
+  
+  it "Comprobar Fecha de Publicacion" do
+    expect(@libro.fecha).to eq('Junio 21, 2014')
+  end
+  
+  it "Comprobar Autor" do
+    expect(@libro.autor).to eq(['Carmen Trujillo'])
+  end
+  
+  it "Comprobar Titulo" do
+    expect(@libro.titulo).to eq('Programacion y Git')
+  end
+  
+  it "Comprobar Editorial" do
+    expect(@libro.editorial).to eq('Almac')
+  end
+  
+  it "Comprobar Edicion" do
+    expect(@libro.edicion).to eq('Tercera')
+  end
+  
+  it "Comprobar Serie" do
+    expect(@libro.serie).to eq('Programacion 1')
+  end
+  
+  it "Comprobar Identificador" do
+    expect(@libro.isbn).to eq('278-553')
+  end
+  
+end
